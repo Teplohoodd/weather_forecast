f138e4bf509724f39696b657f0b0c7b0c9bc5875
Teplohoodd <teplohood@1hokage.ru>
2024-12-03 22:19:27 +0300

    Added check_bad_weather function and asyncio usage. Also added better output and function handler

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
 import requests
+import asyncio
 from json import *
 import flask
 
@@ -18,19 +19,67 @@ location_key = data["Key"]
 forecast_url = f"http://dataservice.accuweather.com/forecasts/v1/daily/1day/{location_key}"
 weather_url = f"http://dataservice.accuweather.com/currentconditions/v1/{location_key}"
 #print(f"Ключ локации: {location_key}")
-def basic_weather(weather_url, params):
+async def basic_weather(weather_url, params):
     response = requests.get(weather_url, params=params)
     weather_data = response.json()
+    discription = weather_data[0]['WeatherText']
+    temp = weather_data[0]['Temperature']['Metric']['Value']
+    humidity = weather_data[0]["RelativeHumidity"]
+    wind_speen = weather_data[0]["Wind"]["Speed"]["Metric"]["Value"]
     #print(weather_data)
-    return f"Текущая погода: {weather_data[0]['WeatherText']}, {weather_data[0]['Temperature']['Metric']['Value']}{weather_data[0]['Temperature']['Metric']['Unit']}\n\
-Относительная влажность: {weather_data[0]["RelativeHumidity"]} %\n Скорость ветра: {weather_data[0]["Wind"]["Speed"]["Metric"]["Value"]} {weather_data[0]["Wind"]["Speed"]["Metric"]["Unit"]}\n"
+    return [discription, temp, humidity, wind_speen]
 
-def precipitation_probab(params, forecast_url):
+async def precipitation_probab(params, forecast_url):
 
     response = requests.get(forecast_url, params=params)
     forecast_data = response.json()
     daily_forecast = forecast_data["DailyForecasts"][0]
     precipitation_probability = daily_forecast["Day"]["PrecipitationProbability"]
-    return f"Вероятность осадков сегодня: {precipitation_probability}%\n"
+    return precipitation_probability
 
-print(basic_weather(weather_url, params), precipitation_probab(params, forecast_url))
+
+def check_bad_weather(weather, precipitation):
+    bad = False
+
+    if weather[1] >= 60 or weather[1] <= -50:
+        print("Нереалистичное значение температуры")
+    else:
+        if weather[1] < -10 or weather[1] > 30:
+            bad = True
+
+    if weather[2] > 100 or weather[2] < 0:
+        print("Нереалистичное значение влажности")
+    else:
+        if weather[2] < 20 or weather[2] > 70:
+            bad = True
+
+    if weather[3] < 0:
+        print("Нереалистичное значение скорости ветра")
+    else:
+        if weather[3] > 30:
+            bad = True
+
+    if precipitation < 0 or precipitation > 100:
+        print("Нереалистичное значение вероятности осадков")
+    else:
+        if precipitation > 40:
+            bad = True
+    return bad
+
+
+
+
+async def main():
+    weather = await basic_weather(weather_url, params)
+    precipitation = await precipitation_probab(params, forecast_url)
+    print("Текущая погода: ", weather[0])
+    print("Текущая температура: ", weather[1])
+    print("Относительная влажность: ", weather[2])
+    print("Скорость ветра: ", weather[3])
+    print("Вероятность осадков сегодня:", precipitation, "%\n")
+    
+    if check_bad_weather == False:
+        print("Погодные условия - супер!")
+    else:
+        print("Брат, посиди дома, не испытывай судьбу..")
+asyncio.run(main())
